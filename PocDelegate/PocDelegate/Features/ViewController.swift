dbd551f20379b3bb38e794e0567fefe58762d681
Felipe Assis <felipeassis97@gmail.com>
2023-12-07 09:15:32 -0300

    feat:Initial structure project

---
+++ PocDelegate/PocDelegate/Features/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PocDelegate
+//
+//  Created by Felipe Assis on 07/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    
+    private func setupView() {
+        view.backgroundColor = .green
+    }
+    
+    private func setupConstraints() {}
+
+
+}
+
